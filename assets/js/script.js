51a1ba6793105704149cf220d0641784a886b75a
Qasim Fida <qasimfida0054@gmail.com>
2024-04-11 23:12:35 +0500

    updated page name and added scroll to top behaviour

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,41 +11,48 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   /* SOUND PAGE AUDIOS */
-  document.querySelectorAll('.player').forEach((player) => {
-    const audio = player.querySelector('audio');
-    const playButton = player.querySelector('.play-btn');
-    const pauseButton = player.querySelector('.pause-btn');
-    const durationDisplay = player.querySelector('.duration');
+  document.querySelectorAll(".player").forEach((player) => {
+    const audio = player.querySelector("audio");
+    const playButton = player.querySelector(".play-btn");
+    const pauseButton = player.querySelector(".pause-btn");
+    const durationDisplay = player.querySelector(".duration");
 
     function formatDuration(value) {
       const minute = Math.floor(value / 60);
       const second = Math.floor(value % 60);
       return minute + ":" + (second < 10 ? "0" + second : second);
     }
-    console.log({audio, player})
-    audio.addEventListener('loadedmetadata', function() {
+    console.log({ audio, player });
+    audio.addEventListener("loadedmetadata", function () {
       durationDisplay.textContent = formatDuration(audio.duration);
     });
 
-    player.addEventListener('click', function(e) {
-      e.stopPropagation()
-      document.querySelectorAll('audio').forEach((el) => {
-        if(el !== audio) {
+    player.addEventListener("click", function (e) {
+      e.stopPropagation();
+      document.querySelectorAll("audio").forEach((el) => {
+        if (el !== audio) {
           el.pause();
-          el.parentElement.querySelector('.pause-btn').classList.add('hide')
-          el.parentElement.querySelector('.play-btn').classList.remove('hide')
+          el.parentElement.querySelector(".pause-btn").classList.add("hide");
+          el.parentElement.querySelector(".play-btn").classList.remove("hide");
         }
       });
       audio.play();
-      pauseButton.classList.remove('hide')
-      playButton.classList.add('hide')
+      pauseButton.classList.remove("hide");
+      playButton.classList.add("hide");
     });
 
-    pauseButton.addEventListener('click', function(e) {
-      e.stopPropagation()
+    pauseButton.addEventListener("click", function (e) {
+      e.stopPropagation();
       audio.pause();
-      pauseButton.classList.add('hide')
-      playButton.classList.remove('hide')
+      pauseButton.classList.add("hide");
+      playButton.classList.remove("hide");
     });
   });
+
+  /* SCROLL TO TOP */
+  const scrollTopBtn = document.getElementById("scroll-top");
+  scrollTopBtn.addEventListener("click", scrollTop);
+  function scrollTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
 });
